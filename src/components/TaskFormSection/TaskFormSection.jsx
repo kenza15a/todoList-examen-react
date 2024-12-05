4afc5acc881de41fac28dea89c00af7867ea73b3
Kenza Filali <filali1991.univ@gmail.com>
2024-12-05 13:07:57 +0100

    desktop/section form working and styles

---
+++ src/components/TaskFormSection/TaskFormSection.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import CategoriesForm from "../Forms/CategoriesForm";
+import CategoryFilter from "../CategoryFilter/CategoryFilter";
+import TasksForm from "../Forms/TasksForm";
+import TasksList from "../TasksList/TasksList";
+import tasksData from "../../data/tasks";
+const TaskFormSection = () => {
+  const [tasks, setTasks] = useState(tasksData);
+  const [categories, setCategories] = useState(["Work", "Home", "Leisure"]);
+  const [filter, setFilter] = useState("");
+  const addCategory = (category) => setCategories([...categories, category]);
+
+  const addTask = (newTask) => setTasks([...tasks, newTask]);
+
+  const deleteTask = (index) => setTasks(tasks.filter((_, i) => i !== index));
+
+  const toggleTask = (index) => {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const filteredTasks =
+    filter === "" ? tasks : tasks.filter((task) => task.category === filter);
+
+  return (
+    <section
+      id="#tasksForm"
+      name="tasksForm"
+      className="  flex flex-col gap-4 items-center justify-center py-12 min-h-max w-[50vw] "
+    >
+      <h1 className="font-bold text-xl">Ma To-Do List par Catégories</h1>
+      <CategoriesForm onAddCategory={addCategory} />
+      <CategoryFilter categories={categories} onFilterChange={setFilter} />
+      <TasksForm onAddTask={addTask} categories={categories} />
+      <TasksList
+        tasks={filteredTasks}
+        onDeleteTask={deleteTask}
+        onToggleTask={toggleTask}
+      />
+    </section>
+  );
+};
+
+export default TaskFormSection;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import TaskFormSecion from "../components/TaskFormSection/TaskFormSecion";
-
+import TaskFormSection from "../components/TaskFormSection/TaskFormSection";
 
 const HomePage = () => {
-  
   return (
     <div className=" flex flex-col min-h-screen">
       <Navbar />
       <main className="bg-white flex-grow w-full justify-center items-center flex">
-        <TaskFormSecion/>
-       
+        <TaskFormSection />
       </main>
       <Footer />
     </div>
@@ -19,42 +15,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-/* const HomePage = () => {
-    const [tasks, setTasks] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [filter, setFilter] = useState("");
-  
-    const addCategory = (category) => setCategories([...categories, category]);
-  
-    const addTask = (newTask) => setTasks([...tasks, newTask]);
-  
-    const deleteTask = (index) => setTasks(tasks.filter((_, i) => i !== index));
-  
-    const toggleTask = (index) => {
-      setTasks(
-        tasks.map((task, i) =>
-          i === index ? { ...task, completed: !task.completed } : task
-        )
-      );
-    };
-  
-    const filteredTasks =
-      filter === "" ? tasks : tasks.filter((task) => task.category === filter);
-  
-    return (
-      <div className="app">
-        <h1>Ma To-Do List par Catégories</h1>
-        <CategoriesForm onAddCategory={addCategory} />
-        <CategoryFilter categories={categories} onFilterChange={setFilter} />
-        <TasksForm onAddTask={addTask} categories={categories} />
-        <TasksList
-          tasks={filteredTasks}
-          onDeleteTask={deleteTask}
-          onToggleTask={toggleTask}
-        />
-      </div>
-    );
-  };
-  
-  export default HomePage;
-   */
