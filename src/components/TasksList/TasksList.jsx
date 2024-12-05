5de0c6874b1f83ae2bce2b97f7ac6229a3b1dedb
Kenza Filali <filali1991.univ@gmail.com>
2024-12-05 13:22:28 +0100

    desktop/all styles done

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import Button from "../Button/Button";
 
 const TasksList = ({ tasks, onDeleteTask, onToggleTask }) => {
-  return (
-    <ul className="task-list">
+  return tasks.length === 0 ? (
+    <p>aucune tâche</p>
+  ) : (
+    <ul className=" w-full flex flex-col gap-4 ">
       {tasks.map((task, index) => (
-        <li key={index} className="task-item">
+        <li key={index} className="p-2 flex flex-row justify-between border-[#DDDDDD] border-[1px] rounded-sm w-full   font-bold">
+          <div className="flex flex-row gap-4 items-center">
           <input
             type="checkbox"
             checked={task.completed}
             onChange={() => onToggleTask(index)}
           />
-          <span className={task.completed ? "line-through" : ""}>
-            {task.task} ({task.category})
+          <span className={task.completed ? "line-through text-[#DDDDDD] " : ""}>
+            {task.task} <span className="ml-4 italic">({task.category})</span>
           </span>
-          <button onClick={() => onDeleteTask(index)} className="btn-delete">
-            Supprimer
-          </button>
+          </div>
+          <Button buttonText="Supprimer" onClick={() => onDeleteTask(index)} />
+          
         </li>
       ))}
     </ul>
